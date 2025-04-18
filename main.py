013963bf60f37a279e6d85df02e5a1e68f698a31
paul-masaharu-rosales <paul.masaharu.rosales@gmail.com>
2025-04-18 03:10:06 -0700

    Fortune Teller Assignment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import os
 app = Flask(__name__)
 
@@ -8,10 +8,21 @@ def home():
 
 @app.route("/about")
 def aboutMe():
-    return """<p>I am currently a student at North Seattle College and currently persuing a Full Stack Development 
+    return """<p>I am currently a student at North Seattle College and currently pursuing a Full Stack Development 
     certificate. I am currently not sure what I want to do after that but I am keeping my eyes and options open. I grew
     up in Seattle and really love living here. I like to go to parks and beaches, most commonly I will go to Discovery Park 
     or will see some of the japanese gardens that are in south Seattle.</p>"""
+
+@app.route("/fortune")
+def fortuneTeller():
+    user = request.args.get('user')
+    color = request.args.get('color')
+    number = request.args.get('number')
+    return render_template('assignment3.html', username = user, colorPicked = color, numberPicked = number)
+
+
+
 if __name__ == '__main__':
     app.run(port=os.getenv("PORT", default=5000))
 # to jump back into the env just run : source venv/bin/activate
+# to get out of env type in terminal deactivate
