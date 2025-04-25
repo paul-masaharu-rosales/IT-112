22c9d6dfada665d66b0958726d46ba357325f037
paul-masaharu-rosales <paul.masaharu.rosales@gmail.com>
2025-04-24 18:06:59 -0700

    partially complete with sql database assignement

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 import os
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+
+
 
 @app.route('/')
 def home():
@@ -20,7 +24,44 @@ def fortuneTeller():
     number = request.args.get('number')
     return render_template('assignment3.html', username = user, colorPicked = color, numberPicked = number)
 
+db = SQLAlchemy(app)
+class Cars(db.Model):
+    id = db.Column('car_id', db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    brand = db.Column(db.String(100))
+    mpg = db.Column(db.Float(10))
+    cost = db.Column(db.Float(100))
 
+    def __init__(self, name, brand, mpg, cost):
+        self.name = name
+        self.brand = brand
+        self.mpg = mpg
+        self.cost = cost
+
+
+
+@app.route('/mydatabase', methods = ['GET', 'POST'])
+def showMyDatabase():
+    if request.method == 'POST':
+        return render_template('assignment4.html', cars = Cars.query.all())
+    else:
+        db.drop_all()
+    db.create_all()
+
+    mustang = Cars("Mustang", "Ford", 15.0, 23000)
+    viper = Cars("Viper", "Dodge", 11.0, 175000)
+    zr1 = Cars("ZR1", "Corvette", 13.0, 125000)
+    turbo911 = Cars("911 Turbo", "Porche", 22.0, 225000)
+
+    db.session.add(mustang)
+    db.session.add(viper)
+    db.session.add(zr1)
+    db.session.add(turbo911)
+    db.session.commit()
+    return render_template('assignment4.html', cars = Cars.query.all())
+
+  #need to add link to details next to each item  
+    
 
 if __name__ == '__main__':
     app.run(port=os.getenv("PORT", default=5000))
